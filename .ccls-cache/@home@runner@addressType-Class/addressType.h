f9ed0b7ce59266987567f60c17582ed04fe57e6a
GeoNemo95 <pocketfish22@gmail.com>
2024-09-17 04:45:34 +0000

    Inital Commit

--- .ccls-cache/@home@runner@addressType-Class/addressType.h
+++ .ccls-cache/@home@runner@addressType-Class/addressType.h
@@ -1,15 +1,15 @@
 #include <string>
 using namespace std;
 
-class Address_Type{
+class addressType{
   private:
     string street;
     string city;
     string state;
     int zip;
   public:
-    Address_Type();
-    Address_Type(string street = "", string city = "", string state "XX", string zip = 10000);
+    addressType();
+    addressType(string street = "", string city = "", string state "XX", string zip = 10000);
     void setStreet(string street);
     void setCity(string city);
     void setState(string state);
