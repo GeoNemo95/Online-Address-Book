6a9a9e64ed187e78eaffeb9b1f16847bf52d3b6b
GeoNemo95 <pocketfish22@gmail.com>
2024-09-23 23:30:30 +0000

    Final

--- .ccls-cache/@home@runner@addressType-Class/addressType.h
+++ .ccls-cache/@home@runner@addressType-Class/addressType.h
@@ -8,15 +8,17 @@ class addressType{
     string state;
     int zip;
   public:
-    addressType();
-    addressType(string street = "", string city = "", string state "XX", string zip = 10000);
+    void setAddress(string, string, string, int){
+      
+    };
     void setStreet(string street);
     void setCity(string city);
     void setState(string state);
-    void setZip(string zip);
+    void setZipcode(string zip);
     string getStreet();
     string getCity(); 
     string getState();
     int getZip();
     void print(ostream &out);
+    addressType(string street = "", string city = "", string state = "XX", int zip = 10000);
 };
--- addressType.h
+++ addressType.h
@@ -8,15 +8,37 @@ class addressType{
     string state;
     int zip;
   public:
-    addressType();
-    addressType(string street = "", string city = "", string state "XX", string zip = 10000);
-    void setStreet(string street);
-    void setCity(string city);
-    void setState(string state);
-    void setZip(string zip);
-    string getStreet();
-    string getCity(); 
-    string getState();
-    int getZip();
-    void print(ostream &out);
+    addressType(string xstreet = "", string xcity = "", string xstate = "XX", int xzip = 10000): street(xstreet), city(xcity), state(xstate), zip(xzip){};
+    void setAddress(string xstreet, string xcity, string xstate, int xzip){
+      street = xstreet;
+      city = xcity;
+      state = xstate;
+      zip = xzip;
+    };
+    void setStreet(string xstreet){street = xstreet;}
+    void setCity(string xcity){city = xcity;}
+    void setState(string xstate){
+      if(xstate.length() != 2){
+        state = "XX";
+      }
+      else{
+        state = xstate;
+      }
+    }
+    void setZipcode(int xzip){
+      if (xzip > 11111 && xzip < 99999){
+        zip = xzip;
+      }
+      else {
+        zip = 10000;
+      }
+    }
+    string getStreet(){return street;}
+    string getCity(){return city;}
+    string getState(){return state;}
+    int getZip(){return zip;}
+    void print(){
+      cout << street << endl << city << ", " << state << " " << zip << endl;
+    }
+    
 };
