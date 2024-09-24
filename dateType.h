9f206ec945316690f0e7619ba8ec9d3b754c4cd4
GeoNemo95 <pocketfish22@gmail.com>
2024-09-24 00:58:22 +0000

    February issues

--- dateType.h
+++ dateType.h
@@ -10,7 +10,44 @@ class dateType{
   public:
     dateType(int month = 1, int day = 1, int year = 1900) : dMonth(month), dDay(day), dYear(year){};
     void setDate(int month, int day, int year){
-      
+      if (month > 12 || month < 1 || day > 31 || day < 1 || year < 1900){
+        dMonth = 1;
+        dDay = 1;
+        dYear = 1900;
+      }
+      else if (int getDaysinMonth = 30){
+        if (day > 30){
+          dDay = 1;
+        }
+        else{
+          dMonth = month;
+          dDay = day;
+          dYear = year;
+        }
+      }
+      else if (int getDaysinMonth = 29){
+        if (day > 29)
+          dDay = 1;
+        else{
+          dMonth = month;
+          dDay = day;
+          dYear = year;
+        }
+      }
+      else if (int getDaysinMonth = 28){
+        if (day > 28)
+          dDay = 1;
+        else{
+          dMonth = month;
+          dDay = day;
+          dYear = year;
+        } 
+      }
+      else{
+        dMonth = month;
+        dDay = day;
+        dYear = year;
+      }
     };
     int getDay(){return dDay;}
     int getMonth(){return dMonth;}
