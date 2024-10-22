2bed967dcabec176407373c903c89cbf1dc69d71
GeoNemo95 <pocketfish22@gmail.com>
2024-10-22 08:31:21 +0000

    Final

--- extPersonType.h
+++ extPersonType.h
@@ -15,9 +15,7 @@ class extPersonType {
     string phoneNumber; 
     string relationship;
   public:
-    extPersonType(string first = "", string last = "", int month = 1, int day = 1, int year = 1900, string street = "", string city = "", string state = "XX", int zip = 10000, string phone = "", string rel = "None") : person(first, last), address(street, city, state, zip), birthdate(month, day, year), phoneNumber(phone), relationship(rel){
-      
-    };
+    extPersonType(string first = "", string last = "", int month = 1, int day = 1, int year = 1900, string street = "", string city = "", string state = "XX", int zip = 10000, string phone = "", string rel = "None") : person(first, last), address(street, city, state, zip), birthdate(month, day, year), phoneNumber(phone), relationship(rel){};
     void setPhoneNumber(string phone){phoneNumber = phone;}
     void setRelationship(string rel){relationship = rel;}
     string getPhoneNumber(){return phoneNumber;}
